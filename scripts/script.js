9ef63b7cfbcfc1cfebb926f462744ba00983059f
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-08-13 16:36:57 -0300

    refatora o desafio 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 window.onload = function createPixelArt() {
-  const div = document.createElement('div');
-  const h1 = document.createElement('h1');
-  div.id = 'div';
-  h1.id = 'title';
-  h1.innerHTML = 'Paleta de Cores';
-  document.body.appendChild(div);
-  div.appendChild(h1);
+  function createH1() {
+    const h1 = document.createElement('h1');
+    const text = document.createTextNode('Paleta de Cores');
+    h1.id = 'title';
+    h1.appendChild(text);
+    document.body.appendChild(h1);
+  }
+
+  createH1();
 };
